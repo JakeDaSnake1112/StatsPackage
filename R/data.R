f49707ba861e48288dffb7536c1b19c9ac37d865
JakeDaSnake1112 <jakew111299@gmail.com>
2021-10-18 23:32:54 -0500

    Adding the first data files, along with the myncurve() function.

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Damage from fire, related to distance
+#'
+#' A dataset containing the distance form a fire station and the corresponding
+#' damage in dollars.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{Distance from fire station in miles}
+#'   \item{DAMAGE}{Fire damage in thousands of dollars}
+#'
+#' }
+#' @source Mendenhall and Schmidt
+"fire"
---
+++ R/myncurve.R
@@ -0,0 +1,24 @@
+#' My Normal Curve
+#'
+#' \code{myncurve} creates a curve showing a normal density distribution, then calculates the area under the curve and graphically represents it with a polygon.
+#'
+#' @param mu The expected value
+#' @param sigma The standard deviation
+#' @param a What point we want to evaluate the area less than
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'@usage myncurve(mu, sigma, a)
+#' @examples
+#' myncurve(3, 1, 4)
+myncurve = function(mu, sigma,a){
+  curve(dnorm(x,mean=mu,sd=sigma), xlim = c(mu-3*sigma, mu + 3*sigma))
+  xc = seq(a-(10*sigma),a, length=1000)
+  yc = dnorm(xc,mu,sigma)
+  polygon(c(a-(10*sigma),xc,a),c(0,yc,0), col = "gray")
+  p = pnorm(a, mu, sigma)
+  p = round(p,4)
+  list(p)
+}
